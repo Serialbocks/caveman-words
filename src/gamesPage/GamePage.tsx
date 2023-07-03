53ecb64d326fd93f9e567f153c9bf5b304df7f49
Serialbocks <john@serialbocks.com>
2023-07-03 16:55:48 -0400

    Add player name

--- src/gamesPage/GamePage.tsx
+++ src/gamesPage/GamePage.tsx
@@ -3,12 +3,38 @@ import {MOCK_GAMES} from './MockGames';
 import { Game } from './Game';
 import "./game.css";
 
+import { uniqueNamesGenerator, adjectives, animals, Config } from 'unique-names-generator';
+
+const characterConfig: Config = {
+    dictionaries: [adjectives, adjectives, animals],
+      length: 3,
+      separator: '',
+      style: 'capital'
+    };
+
 class GamesPage extends React.Component
 {
   public GamesPage() {
   }
 
-  readonly state: any = {};
+  readonly state: any = {
+    playerName: this.getPlayerName()
+  };
+
+  getPlayerName() {
+    var currentName = localStorage.getItem("playerName");
+    if(!currentName) {
+      currentName = uniqueNamesGenerator(characterConfig);
+      localStorage.setItem("playerName", currentName);
+    }
+
+    return currentName;
+  }
+
+  setPlayerName(name: string) {
+    localStorage.setItem("playerName", name);
+    this.setState({playerName: name});
+  }
 
   GameList({ games }: any) {
     const gameListItems = games.map((game: Game) => (
@@ -29,10 +55,19 @@ class GamesPage extends React.Component
   render() {
     return (
       <>
+        <div className="row">
+          <div className="col-sm-4 align-center">
+              <label htmlFor="playerName">Player Name</label>
+          </div>
+          <div className="col-sm-8">
+              <input value={this.state.playerName} type="text" id="playerName" onChange={(e)=>{this.setPlayerName(e.target.value)}} />
+          </div>
+        </div>
+
         <h1>Games</h1>
         <this.GameList games={MOCK_GAMES} />
         <div className="flex-container">
-          <a href={`/create`}><button style={{width: "183px"}}>Create Game</button></a>
+          <a href={`/create`}><button className="primary" style={{width: "183px"}}>Create Game</button></a>
         </div>
       </>
     );
