ae05d7f7fed4e8c685215c76d0f306efc085f43d
Serialbocks <john@serialbocks.com>
2023-08-28 14:41:01 -0400

    Add endpoint for pi to ping

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var log = require('../utils/log')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html', { title: 'Express' });
+});
+
+router.get('/tcping', function(req, res, next) {
+  log(`TCPing ${req.query.version}`);
+  res.json(true);
 });
 
 module.exports = router;
